ac0a484e81e97e8c1515e1f49dec803179520a69
bardaev <badma3581@gmail.com>
2022-08-21 22:21:22 +0300

    dev10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -4,22 +4,22 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"time"
 )
 
 func main() {
-	// go StartServer()
 	var timeout int = *flag.Int("timeout", 10, "Timeout in seconds")
 	flag.Parse()
 
-	// var host string = flag.Arg(0)
-	// var port string = flag.Arg(1)
-	var host string = "217.65.3.21"
-	var port string = "80"
+	var host string = flag.Arg(0)
+	var port string = flag.Arg(1)
+	// var host string = "217.65.3.21"
+	// var port string = "80"
 
-	conn, err := net.Dial("tcp", host+":"+port)
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), time.Duration(timeout)*time.Second)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -28,45 +28,28 @@ func main() {
 	defer conn.Close()
 
 	sc := bufio.NewScanner(os.Stdin)
-	// httpRequest := "GET /"
 
 	for sc.Scan() {
 		var req string = sc.Text()
-		// var req string = "GET /"
-		conn.SetWriteDeadline(time.Now().Add(time.Second * time.Duration(timeout)))
-		_, err := conn.Write([]byte(req))
+		resRq := req + " / HTTP/1.0\r\n\r\n"
+		_, err := conn.Write([]byte(resRq))
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		buf, err := bufio.NewReader(conn).ReadString('\n')
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(buf)
-	}
-}
-
-func StartServer() {
-	message := "Hello, I am a server" // отправляемое сообщение
-	listener, err := net.Listen("tcp", ":4545")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer listener.Close()
-	fmt.Println("Server is listening...")
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		conn.Write([]byte(message))
-		conn.Close()
+		go func() {
+			buf := bufio.NewReader(conn)
+			for {
+				resp, err := buf.ReadString('\n')
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					continue
+				}
+				fmt.Print(resp)
+			}
+		}()
 	}
 }
